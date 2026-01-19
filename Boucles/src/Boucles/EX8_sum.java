19c2f51108bb78052754cccc3cf9fa9c0375be19
Eddahmani Aya <edaya816@gmail.com>
2026-01-19 21:45:38 +0100

    💻 TP_Java_Basic

---
+++ Boucles/src/Boucles/EX8_sum.java
@@ -0,0 +1,17 @@
+package Boucles;
+/*Exercice 8 :
+Utilisez une boucle while pour calculer et afficher la somme des nombres de 1
+à 50.
+*/
+public class EX8_sum {
+	public static void main(String[] args) {
+		int sum = 0,i = 0;
+		while(i<=50) {
+			sum=sum+i;
+			i++;
+		}
+		System.out.print("la somme des nombres de 1 à 50 est:"+sum);
+		System.out.print("😂");
+		
+	}
+}
